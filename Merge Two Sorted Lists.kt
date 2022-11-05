fdabb37e73a4a5644641c5ce42efd037ea293f27
Servando Negrete <servandoo.12@hotmail.com>
2022-11-05 14:42:56 -0600

    first commit

---
+++ Merge Two Sorted Lists.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        var result: ListNode? = ListNode(0)
+        var current = result
+        
+        var node1 = l1
+        var node2 = l2
+        while (node1 != null || node2 != null) {
+            if (node1 == null) {
+                current?.next = node2
+                break
+            }
+            if (node2 == null) {
+                current?.next = node1
+                break
+            }
+
+            if (node1?.`val` < node2?.`val`) {
+                current?.next = node1
+                node1 = node1?.next
+            } else {
+                current?.next = node2
+                node2 = node2?.next
+            }
+            current = current?.next
+        }
+        return result?.next
+    }
+}
