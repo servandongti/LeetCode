2edc551cdadd7ceb1d0ca2cb738a410f7f6b1adc
Servando Negrete <servandoo.12@hotmail.com>
2022-11-05 15:12:47 -0600

    first commit

---
+++ Remove Duplicates from Sorted Array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let i=0;
+    for (let j=i+1; j<nums.length; j++) {
+        if (nums[i]===nums[j]) {
+            nums.splice(j,1);
+            j--;
+        }else {
+            i=j
+        } 
+    }
+    return nums.length;
+};
