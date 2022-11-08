ac41efd2defa9af14d1eba07618cdbe492f73c58
Servando Negrete <servandoo.12@hotmail.com>
2022-11-07 23:20:05 -0600

    first commit

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                # Find the index of the first element >= x
+                idx = bisect_left(sub, x)
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
